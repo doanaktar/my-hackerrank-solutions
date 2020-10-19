950ef3b488bcf68f90048b524716581876b97eed
Doğan Aktar <66390064+doganaktarr@users.noreply.github.com>
2020-10-19 16:30:05 +0300

    Solution

---
+++ 30 Days Of Code/Day 17 - More Exceptions/Day 17.py
@@ -0,0 +1,18 @@
+#Write your code here
+import math
+class Calculator():
+    def power(self, n, p):
+        if n < 0 or p < 0:
+            return "n and p should be non-negative"
+        else:
+            return int(math.pow(n,p))
+            
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
